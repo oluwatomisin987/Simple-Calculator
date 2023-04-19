6324228ededb3094cf6473fe19c6f0d10df78afa
OLUWATOMISIN <hardex.tommy@gmail.com>
2023-04-19 15:26:58 +0100

    19-04

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const screen = document.querySelector(".screen");
 const equalButton = document.querySelector(".equal");
 const deleteButton = document.querySelector(".btn-del");
 const resetButton = document.querySelector(".btn-reset");
+const toggleBg = document.querySelector('.tri-state-toggle')
+const toggleBtn = document.querySelector('.butto');
+const firstDiv = document.querySelector('.first-div')
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -50,20 +53,65 @@ arr.forEach((element, index) => {
     if (index == 0) {
       document.getElementsByTagName("body")[0].style.backgroundColor =
         "hsl(222,26%,31%)";
-      // buttons.style.background = "red";
-      equalButton.style.backgroundColor = "yellow";
-      screen.style.backgroundColor = "hsl(224, 36%, 15%)";
+        screen.style.backgroundColor = 'hsl(224, 36%, 15%)'
+        screen.style.color = 'hsl(0, 0%, 100%)'
+     
+      equalButton.style.backgroundColor = "hsl(6, 63%, 50%)";
+      equalButton.style.boxShadow = "hsl(6, 70%, 34%)";
+      deleteButton.style.backgroundColor = 'hsl(225, 21%, 49%)';
+      deleteButton.style.boxShadow = '2px 3px  hsl(224, 28%, 35%)'
+      resetButton.style.backgroundColor = "hsl(225, 21%, 49%)";
+      resetButton.style.boxShadow = "2px 3px  hsl(224, 28%, 35%)";
+      buttons.forEach(btn => (btn.style.backgroundColor = 'hsl(334, 25%, 89%'));
+      buttons.forEach(btn => (btn.style.boxShadow = '2px 3px hsl(28, 16%, 65%'));
+      toggleBg.style.backgroundColor = "hsl(223, 31%, 20%)";
+      toggleBtn.style.backgroundColor = 'red';
+      firstDiv.style.color = 'white';
+
+
+       
     } else if (index == 1) {
-      document.getElementsByTagName("body")[0].style.backgroundColor = "teal";
-      equalButton.style.backgroundColor = "blue";
-      screen.style.backgroundColor = "pink";
-      //  buttons.style.backgroundColor = "hsl(225, 8%, 89%)";
-    } else {
       document.getElementsByTagName("body")[0].style.backgroundColor =
-        "rgb(92, 204, 125)";
-      equalButton.style.backgroundColor = "green";
-      screen.style.backgroundColor = "orange";
-      //  buttons.style.backgroundColor = "hsl(225, 91%, 99%)";
+        "hsl(0, 0%, 90%)";
+      screen.style.backgroundColor = "hsl(0, 0%, 93%)";
+      screen.style.color = "hsl(60, 10%, 19%)";
+
+      equalButton.style.backgroundColor = "hsl(25, 98%, 40%)";
+      equalButton.style.boxShadow = "hsl(25, 99%, 27%)";
+      deleteButton.style.backgroundColor = "hsl(185, 42%, 37%)";
+      deleteButton.style.boxShadow = "2px 3px  hsl(185, 58%, 25%)";
+     resetButton.style.backgroundColor = "hsl(185, 42%, 37%)";
+     resetButton.style.boxShadow = "2px 3px  hsl(185, 58%, 25%)";
+     buttons.forEach(
+        (btn) => (btn.style.backgroundColor = "hsl(45, 7%, 89%") );
+     buttons.forEach(
+        (btn) => (btn.style.boxShadow = "2px 3px hsl(35, 11%, 61%") );
+         buttons.forEach((btn) => (btn.style.color = "black"));
+    toggleBg.style.backgroundColor = 'hsl(0, 5%, 81%)';
+    toggleBtn.style.backgroundColor = "hsl(25, 98%, 40%";
+     firstDiv.style.color = "black";
+    } 
+    
+    
+    else {
+      document.getElementsByTagName("body")[0].style.backgroundColor =
+        "hsl(268, 75%, 9%)";
+      screen.style.backgroundColor = "hsl(268, 71%, 12%)";
+      screen.style.color = "hsl(52, 100%, 62%)";
+
+      equalButton.style.backgroundColor = "hsl(176, 100%, 44%)";
+      equalButton.style.boxShadow = "hsl(177, 92%, 70%)";
+      deleteButton.style.backgroundColor = "hsl(281, 89%, 26%)";
+      deleteButton.style.boxShadow = "2px 3px  hsl(285, 91%, 52%)";
+       resetButton.style.backgroundColor = "hsl(281, 89%, 26%)";
+       resetButton.style.boxShadow = "2px 3px  hsl(285, 91%, 52%)";
+      buttons.forEach((btn) => (btn.style.backgroundColor = "hsl(268, 47%, 21%"));
+      buttons.forEach(
+        (btn) => (btn.style.boxShadow = "2px 3px hsl(290, 70%, 36%"));
+        buttons.forEach (btn => btn.style.color = 'hsl(52, 100%, 62%)');
+      toggleBg.style.backgroundColor = "hsl(268, 71%, 12%)";
+      toggleBtn.style.backgroundColor = "white";
+       firstDiv.style.color = "hsl(52, 100%, 62%)";
     }
 
     // Then i filtered my array (picking out the elements that were not clicked) then setting their opacity to “0”.
@@ -76,6 +124,16 @@ arr.forEach((element, index) => {
       });
   });
 });
+
+
+
+
+
+
+
+
+
+
 
 // var buttons = document.getElementsByClassName("button");
 // var arr = [...buttons];
@@ -100,6 +158,16 @@ arr.forEach((element, index) => {
 //       });
 //   });
 // });
+
+
+
+
+
+
+
+
+
+
 
 // function graadingStudents (grades){
 
